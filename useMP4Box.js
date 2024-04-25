139c89745a5269e4043c9f48ce7a36db3ae704b2
dukesook <devonsookhoo14@gmail.com>
2024-04-25 13:31:54 -0600

    condense example

--- useMP4Box.js
+++ useMP4Box.js
@@ -3,21 +3,16 @@ console.log('MP4Box: ', MP4Box);
 const fileInput = document.getElementById('input');
 fileInput.onchange = () => {
   const selectedFile = fileInput.files[0];
-  // console.log(selectedFile);
 
   selectedFile.arrayBuffer().then((buf) => {
     const mp4boxfile = MP4Box.createFile();
 
-    // mp4boxfile.onReady = function (info) {
-    //   console.log("Received File Information", info);
-    // }
+    mp4boxfile.onReady = function (info) {
+      // console.log('Received File Information', info);
+    };
 
-    // MP4Box needs each buffer to have a custom `fileStart` property,
-    // supposedly telling which slice of the file this ArrayBuffer refers to.
-    buf.fileStart = 0;
-
+    buf.fileStart = 0; // MP4Box needs each buffer to have a custom `fileStart` property, supposedly telling which slice of the file this ArrayBuffer refers to.
     mp4boxfile.appendBuffer(buf);
-
     mp4boxfile.flush();
 
     console.log(mp4boxfile);
